cd3d0b207299f80c093e44c4e44f67b37141cd71
Celeiro Softwares <celeirosoftwares@gmail.com>
2026-04-27 10:15:27 -0300

    fix: redirect raiz para LP estatica

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Necessário para o Mapbox GL funcionar no servidor
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'mapbox-gl': 'mapbox-gl',
-    }
-    return config
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index-lp.html',
+        permanent: false,
+      },
+    ]
   },
 }
 
